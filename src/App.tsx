ade28757abd595cc999bc4a26eebbbb6204e7e8c
frontcodelover <contact@avantjetaisriche.com>
2023-03-14 17:19:37 +0100

    TEST: add testing library

--- src/App.tsx
+++ src/App.tsx
@@ -3,35 +3,35 @@ import reactLogo from './assets/react.svg';
 import './App.css';
 import { db } from './database/firebase';
 import { collection, query, where, getDocs } from 'firebase/firestore';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
-  const [count, setCount] = useState(0);
-  const [data, setData] = useState<{}>([]);
+  // const [count, setCount] = useState(0);
+  // const [data, setData] = useState<{}>([]);
 
-  const getProjects = async () => {
-    try {
-      console.log('coucou');
-      const snapshot = await getDocs(collection(db, 'projects'));
-      console.log(snapshot);
-      const projects = snapshot.docs.map((doc) => doc.data());
-      console.log(projects);
-      setData(projects);
-    } catch (error) {
-      console.log(error);
-    }
-  };
+  // const getProjects = async () => {
+  //   try {
+  //     console.log('coucou');
+  //     const snapshot = await getDocs(collection(db, 'projects'));
+  //     console.log(snapshot);
+  //     const projects = snapshot.docs.map((doc) => doc.data());
+  //     console.log(projects);
+  //     setData(projects);
+  //   } catch (error) {
+  //     console.log(error);
+  //   }
+  // };
 
-  useEffect(() => {
-    getProjects();
-  }, []);
-
-  console.log('DATA >>>>', data);
-  console.log(typeof data);
+  // useEffect(() => {
+  //   getProjects();
+  // }, []);
 
   return (
-    <div className='App'>
-      <h1 className='text-3xl bg-red-400 font-bold underline'>Hello world!</h1>
-    </div>
+    <Router>
+      <Routes>
+        <Route path='/' element={<div>Home</div>} />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/test/Home.test.tsx
@@ -0,0 +1,11 @@
+import Home from '../pages/Home';
+import { describe, test, expect } from 'vitest';
+
+test('Home component should display Bienvenue', () => {
+  const home = Home();
+  expect(home).toHaveProperty('props');
+  expect(home.props).toHaveProperty('children');
+  expect(home.props.children).toHaveProperty('props');
+  expect(home.props.children.props).toHaveProperty('children');
+  expect(home.props.children.props.children).toBe('Bienvenue');
+});
